04aba63bd197c829642e0d863a078bd1c16a03ad
Habib Aroua <habib.a@xelero.io>
2022-09-23 12:05:06 +0100

    use the interface GrapgQl

--- server.js
+++ server.js
@@ -1,24 +1,37 @@
-const schema = require('./schema');
 const express = require('express');
-const graphqlHTTP = require('express-graphql').graphqlHTTP;;
-const cors = require('cors')
-const bodyParser = require('body-parser');
-
-
 const app = express();
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const { graphqlHTTP } = require('express-graphql');
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLObject
+} = require('graphql');
 
-app.use('/graphql',graphqlHTTP({
-    schema,
-    graphql: true
+
+const RootQueryType = new GraphQLObjectType({
+    name: '_rootQuery',
+    description: 'Its a root query graph level',
+    fields: () => ({
+        test: {
+            type: GraphQLString,
+            description: 'Test field',
+            resolve: ()=> {
+                return "Habib Aroua"
+            }
+        }
+    })
+})
+
+const schema = new GraphQLSchema({
+    query: RootQueryType
+})
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
 }))
 
-const PORT = process.env.PORT || 5000 ;
-
-app.listen
-(
-    PORT,
-    () =>console.log(`Server started on port ${PORT}`)
-);
+app.listen(5000, ()=>{
+    console.log('Server is listening on port 5000');
+})
